cb46d4bcebfa269b6fd98e216f580a0b1d3f608f
reload <xreload@163.com>
2022-06-21 10:19:20 +0000

    客户端：完成私聊模式

--- client_pkg/client.go
+++ client_pkg/client.go
@@ -74,10 +74,12 @@ func (client *Client) Run() {
 		case 1:
 			// 公聊模式
 			fmt.Println("公聊模式选择。。。")
+			client.PublicChat()
 			break
 		case 2:
 			// 私聊模式
 			fmt.Println("私聊模式选择。。。")
+			client.PrivateChat()
 			break
 		case 3:
 			// 更新用户名
@@ -95,10 +97,74 @@ func (client *Client) DealResponse() {
 	// 与下面的 for 是相同的作用
 	// 永久阻塞监听的 IO方式   同步IO
 	io.Copy(os.Stdout, client.connfd) // 输出重定向吗？
+	/* for {
+		buf := make([]byte, 4096)
+		client.connfd.Read(buf)
+		fmt.Printf(buf)
+	} */
+}
 
-	// for {
-	// 	buf := make([]byte, 4096)
-	// 	client.connfd.Read(buf)
-	// 	fmt.Printf(buf)
-	// }
+func (client *Client) PublicChat() {
+	// 提升用户输入消息
+	var chatMsg string
+
+	fmt.Println("----------请输入聊天内容，exit退出")
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		// 发送给服务器
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.connfd.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("connfd.Write err:", err)
+				break
+			}
+		}
+
+		fmt.Println("----------请输入聊天内容，exit退出")
+		fmt.Scanln(&chatMsg)
+	}
+}
+
+func (client *Client) SelectUser() {
+	sendMsg := "who\n"
+	_, err := client.connfd.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn write err:", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	client.SelectUser()
+	fmt.Println("-------请输入聊天对象[用户名]：")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println("----------请输入消息内容：")
+		fmt.Scanln(chatMsg)
+
+		for chatMsg != "exit" {
+			// 发送给服务器
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.connfd.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("connfd.Write err:", err)
+					break
+				}
+			}
+
+			fmt.Println("----------请输入聊天内容，exit退出")
+			fmt.Scanln(&chatMsg)
+		}
+
+		client.SelectUser()
+		fmt.Println("-------请输入聊天对象[用户名]：")
+		fmt.Scanln(&remoteName)
+	}
 }
